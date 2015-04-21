1296e9f025013c7bd48a00d398deb4c1ae1b9e42
Marshall Shen <mshen@groupon.com>
2015-04-20 22:01:49 -0500

    WIP scrapping

--- lib/google_play.rb
+++ lib/google_play.rb
@@ -1,9 +1,12 @@
 require_relative 'google_play/version.rb'
 require_relative 'google_play/measure.rb'
 require_relative 'google_play/gmail_service.rb'
+require_relative 'google_play/scraper.rb'
 
 module GooglePlay
-  def self.experiment
-    "run me bae"
+  def self.experiment(account)
+    # account = {login: 'foo', password: 'bar'}
+    scraper = GooglePlayScraper.new(account)
+    scraper.login!
   end
 end
--- lib/google_play/scraper.rb
+++ lib/google_play/scraper.rb
@@ -1,7 +1,7 @@
 require 'capybara/poltergeist'
 require 'yaml'
 
-class InterestScraper
+class GooglePlayScraper
   include Capybara::DSL
 
   attr_accessor :google, :account
@@ -33,84 +33,39 @@ class InterestScraper
 
     google.uncheck 'Stay signed in'
     google.click_on 'Sign in'
-  ensure
-    enable_interest!
   end
 
-  def enable_interest!
-    google.visit(PREFERENCES_ENDPOINT)
-    sleep(1)
-      #google.driver.save_screenshot("#{Time.now.to_s}_before.png", :full => true)
-    begin
-      return if !google.has_selector?(:xpath, "/html/body/div[6]/div/div[3]/div[7]/div[2]/div[2]/div[2]/form[1]/div[1]")
+  GOOGLE_PLAY_MOVIE_URL = "https://play.google.com/store/movies"
 
-      google.find(:xpath, "/html/body/div[6]/div/div[3]/div[7]/div[2]/div[2]/div[2]/form[1]/div[1]").click
-      #google.driver.save_screenshot("#{Time.now.to_s}_after_success.png", :full => true)
-    rescue
-      #google.save_screenshot("#{Time.now.to_s}_after_error.png", :full => true)
-    end
-  end
-
-  def get_interests!
-    google.visit(PREFERENCES_ENDPOINT)
-
-    sleep(1)
-
-    Interests.new.tap do |i|
-      i.on_google      = scrape_interests '/html/body/div[6]/div/div[3]/div[5]/div[2]/div[1]/div[2]/div[1]/div'
-      i.across_the_web = scrape_interests '/html/body/div[6]/div/div[3]/div[5]/div[2]/div[2]/div[2]/div[1]/div'
-    end
-  end
-
-  def scrape_interests(xpath)
-    begin
-      google.find(:xpath, xpath).click
-      google.all('td.Yt').map{ |e| e.text }
-    rescue
-      []
-    end
-  end
-
-  class Interests
-    attr_accessor :on_google, :across_the_web
-  end
-
-  def get_youtube_video_ads(search)
-    query = "https://www.youtube.com/results?search_query=#{search.gsub(/ /, '+')}"
-    google.visit(query)
+  def get_google_play_recommendations
+    google.visit(GOOGLE_PLAY_MOVIE_URL)
     sleep(2)
+    recommendation_xpath = '//*[@id="body-content"]/div[2]/div/div[4]/div/h1/a[1]'
+    google.find(:xpath, recommendation_xpath).click
 
-    video_ad_list_tmp = []
-    video_ad_list = []
+    all_movies = google.all('details') # 'details' class better?
 
-    begin
-      video_ad_list_tmp = google.all('div.pyv-afc-ads-inner').first.all('div.yt-lockup-content')
-      video_ad_list_tmp.each do |ad|
-        title = ad.all('h3.yt-lockup-title').first.text
-        long_url = ad.all('h3.yt-lockup-title').first.all('a').first['href']
-        short_url = long_url.split(/adurl=/).last
-        by = ad.all('div.yt-lockup-byline').first.all('a').first.text
-        description = ad.all('div.yt-lockup-description').first.text
-        video_ad_list.push(
-        {title: title,
-         long_url: long_url,
-         short_url: short_url,
-         by: by,
-         description: description})
-      end
-    rescue
-      puts 'died here'
+    all_movies.map do |movie_dom|
+      get_movie_attributes(movie_dom)
     end
-
-    video_ad_list.each do |ad|
-      ad.each_pair do |k, v|
-        next if k == :long_url
-        puts k.to_s + ": " + v
-      end
-      puts
-    end
-    puts
   end
+
+  # Mark this as TODO, I have to leave soon..
+  # COmmit this and push, I"ll work on it now
+  # https://github.com/marshallshen/xray-google-play/commit/a30aa39f172077a521025825925846a61f42d7c2#diff-ed538b511af23f4531fe39245d8fcc0bR60
+  def get_movies(movie)
+    Movie.new.tap do |m|
+      m.title = google.find('a title') # this won't work because google does not know the scope, yeah, we have to pass the xpath to the title, etc
+      m.genre = google.find('')
+      m.price = google.find('')
+    end
+  end
+
+  class Movie
+    attr_accessor :title, :genre, :price
+  end
+  # 3 dump current state of recommendations somewhere
+  #
 
   def clean!
     google.driver.clear_cookies
